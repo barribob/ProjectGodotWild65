77cb5abacbd8ae30cb173e8caa856123f805147b
Barribob Development <barribobdevelopment@gmail.com>
2024-01-14 13:43:00 -0700

    force away when player damaged

--- Enemy.gd
+++ Enemy.gd
@@ -4,21 +4,33 @@ extends CharacterBody3D
 
 var player
 var speed = 1.0
+var move_lag : float = 16.0
+var force_away_speed = 20.0
 
 func _physics_process(delta):
     if player:
         look_at(player.global_position, Vector3.UP)
-        velocity = -transform.basis.z * speed
+        velocity = lerp(velocity, -transform.basis.z * speed, delta * move_lag)
+    else:
+        velocity = lerp(velocity, Vector2.ZERO, delta * move_lag)
         
     move_and_slide()
 
 func init(in_player):
     player = in_player
+    player.player_damaged.connect(force_away)
+    
+func force_away(params):
+    var distance = (player.global_position - global_position).length()
+    if distance < 5:
+        var dir = (player.global_position - global_position).normalized()
+        velocity += dir * -1 * force_away_speed
 
 func damage(damage_params):
     var item = item_scene.instantiate()
     item.position = position
     get_parent().add_child(item)
+    player.player_damaged.disconnect(force_away)
     queue_free()
 
 func _on_damage_area_area_entered(area):
