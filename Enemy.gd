f8774e4a90c603cde505e92d653af987d1d52343
Barribob Development <barribobdevelopment@gmail.com>
2024-01-14 10:50:31 -0700

    make enemies actually use physics

--- Enemy.gd
+++ Enemy.gd
@@ -8,7 +8,9 @@ var speed = 1.0
 func _physics_process(delta):
     if player:
         look_at(player.global_position, Vector3.UP)
-        transform.origin += -transform.basis.z * speed * delta
+        velocity = -transform.basis.z * speed
+        
+    move_and_slide()
 
 func init(in_player):
     player = in_player
