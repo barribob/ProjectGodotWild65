9d41e656306a5262da17386e11d0b8276e121dde
Barribob Development <barribobdevelopment@gmail.com>
2024-01-14 08:27:30 -0700

    add auto reload when not shooting

--- ShootHandler.gd
+++ ShootHandler.gd
@@ -13,15 +13,17 @@ var clip_size = 5
 var current_ammo: int = 5
 var reload_time = 1.0
 var reload_cooldown = 0.0
+var is_reloading = false
 
 func _process(delta):
     reticle.global_position = Utils.get_3d_mouse_pos(0.1, self, get_viewport().get_camera_3d())
     pivot.look_at(reticle.global_position)
     
-    if current_ammo == 0:
+    if is_reloading:
         reload_cooldown = clampf(reload_cooldown - delta, 0, reload_time)
         if Utils.leq(reload_cooldown, 0):
             current_ammo = clip_size
+            is_reloading = false
     
     shoot_cooldown = clampf(shoot_cooldown - delta, 0, shoot_interval)
     if Input.is_action_pressed("shoot") and Utils.leq(shoot_cooldown, 0) and current_ammo > 0:
@@ -29,8 +31,14 @@ func _process(delta):
         fired.emit()
         shoot_cooldown = shoot_interval
         current_ammo -= 1
+        is_reloading = false
+        reload_cooldown = 0 # cancel any in-progress reloads
         if current_ammo == 0:
             reload_cooldown = reload_time
+            is_reloading = true
+    elif not is_reloading:
+        reload_cooldown = reload_time
+        is_reloading = true        
 
 func shoot():
     var b = projectile.instantiate()
