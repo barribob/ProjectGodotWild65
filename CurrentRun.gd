3a3b908dd890beadbbf5bbd634ef92d46c388fce
Barribob Development <barribobdevelopment@gmail.com>
2024-01-19 08:18:27 -0700

    remove environment for now

--- CurrentRun.gd
+++ CurrentRun.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
 @onready var music = load("res://sounds/Map_Loop_v1.mp3")
+@onready var spawner = $EnemySpawner
 
 func _ready():
-    SoundManager.play_music_at_volume(music, -10)
+    if (!OS.is_debug_build()):
+        SoundManager.play_music_at_volume(music, -10)
+        spawner.start_waves()
